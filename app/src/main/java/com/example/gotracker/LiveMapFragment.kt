22fc1d168957d2989c1d9e7482b63661dc88d87a
OMANTHA <omanthagaming@gmail.com>
2025-08-09 10:20:26 +0530

    initial commit

--- app/src/main/java/com/example/gotracker/LiveMapFragment.kt
+++ app/src/main/java/com/example/gotracker/LiveMapFragment.kt
@@ -5,16 +5,70 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
+class LiveMapFragment : Fragment() , OnMapReadyCallback {
 
-class LiveMapFragment : Fragment() {
+    private lateinit var mMap: GoogleMap
+    private lateinit var database: DatabaseReference
+    private var busMarker: Marker? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_live_map, container, false)
+        val view = inflater.inflate(R.layout.fragment_live_map, container, false)
+
+        // Initialize Firebase
+        database = FirebaseDatabase.getInstance().getReference("bus/location")
+
+        // Initialize Map Fragment
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        return view
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.uiSettings.isZoomControlsEnabled = true
+
+        listenToBusLocation()
+    }
+
+    private fun listenToBusLocation() {
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val lat = snapshot.child("latitude").getValue(Double::class.java)
+                val lng = snapshot.child("longitude").getValue(Double::class.java)
+
+                if (lat != null && lng != null) {
+                    val busLocation = LatLng(lat, lng)
+                    if (busMarker == null) {
+                        busMarker = mMap.addMarker(MarkerOptions().position(busLocation).title("Bus"))
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(busLocation, 16f))
+                    } else {
+                        busMarker!!.position = busLocation
+                        mMap.animateCamera(CameraUpdateFactory.newLatLng(busLocation))
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle error
+            }
+        })
     }
 }
